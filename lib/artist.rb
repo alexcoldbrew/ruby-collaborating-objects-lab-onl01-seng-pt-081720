ccff662b1000c07165afae84d7a8e4ee463b91b5
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-09-18 01:41:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,7 +22,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    self.all.detect { |artist| }
+    self.all.detect { |artist| artist.name == name || }
   end
   
   def print_songs #instance method iterates through @songs of an instance of Artist
